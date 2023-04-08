7aca5d3ceadc812290b8338e2ff3eac749780e09
Aunjrya Fleming <104907548+ParisianMist@users.noreply.github.com>
2023-04-07 22:52:32 -0400

    change employee seed file

--- seeds/seed_shift.js
+++ seeds/seed_shift.js
@@ -12,118 +12,148 @@ exports.seed = async function (knex) {
     //then add these ones
     .then(() => {
       return knex('shift').insert(
-        [//PINK
+        [//Molly Weasley
           {
             'id': uuid.v4(),
-            'description': 'Pink Day #1',
+            'description': 'Day',
             'start_time': '2023-04-01T07:30',
             'end_time': '2023-04-01T19:30',
-            'employee_id': '3db6dc51-b461-4fa4-a6e4-b1ff352221c5'
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
           },
           {
             'id': uuid.v4(),
-            'description': 'Pink Day #2',
+            'description': 'Day',
             'start_time': '2023-04-02T07:30',
             'end_time': '2023-04-02T19:30',
-            'employee_id': '3db6dc51-b461-4fa4-a6e4-b1ff352221c5'
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
           },
           {
             'id': uuid.v4(),
-            'description': 'Pink Night #1',
+            'description': 'Night',
             'start_time': '2023-04-03T19:30',
             'end_time': '2023-04-04T07:30',
-            'employee_id': '3db6dc51-b461-4fa4-a6e4-b1ff352221c5'
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
           },
           {
             'id': uuid.v4(),
-            'description': 'Pink Night #2',
+            'description': 'Night',
             'start_time': '2023-04-04T19:30',
             'end_time': '2023-04-05T07:30',
-            'employee_id': '3db6dc51-b461-4fa4-a6e4-b1ff352221c5'
-          },
-          //green
-          {
-            'id': uuid.v4(),
-            'description': 'Green Day #1',
-            'start_time': '2023-04-03T07:30',
-            'end_time': '2023-04-03T19:30',
             'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
           },
           {
             'id': uuid.v4(),
-            'description': 'Green Day #2',
-            'start_time': '2023-04-04T07:30',
-            'end_time': '2023-04-04T19:30',
-            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
-
-          },
-          {
-            'id': uuid.v4(),
-            'description': 'Green Night #1',
-            'start_time': '2023-04-05T19:30',
-            'end_time': '2023-04-06T07:30',
+            'description': 'Day',
+            'start_time': '2023-04-10T07:30',
+            'end_time': '2023-04-10T19:30',
             'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
           },
           {
             'id': uuid.v4(),
-            'description': 'Green Night #2',
-            'start_time': '2023-04-06T19:30',
-            'end_time': '2023-04-07T07:30',
+            'description': 'Day',
+            'start_time': '2023-04-11T07:30',
+            'end_time': '2023-04-11T19:30',
             'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
-
-          },
-          //PURPLE
-          {
-            'id': uuid.v4(),
-            'description': 'Purple Day #1',
-            'start_time': '2023-04-05T07:30',
-            'end_time': '2023-04-05T19:30',
-            'employee_id': '4c7e6819-a91a-45b2-a454-f931e4a7cce3'
           },
           {
             'id': uuid.v4(),
-            'description': 'Purple Day #2',
-            'start_time': '2023-04-06T07:30',
-            'end_time': '2023-04-06T19:30',
-            'employee_id': '4c7e6819-a91a-45b2-a454-f931e4a7cce3'
+            'description': 'Night',
+            'start_time': '2023-04-12T19:30',
+            'end_time': '2023-04-13T07:30',
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
           },
           {
             'id': uuid.v4(),
-            'description': 'Purple Night #1',
-            'start_time': '2023-04-07T19:30',
-            'end_time': '2023-04-08T07:30',
-            'employee_id': '4c7e6819-a91a-45b2-a454-f931e4a7cce3'
+            'description': 'Night',
+            'start_time': '2023-04-13T19:30',
+            'end_time': '2023-04-14T07:30',
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
+          },{
+            'id': uuid.v4(),
+            'description': 'Day',
+            'start_time': '2023-04-19T07:30',
+            'end_time': '2023-04-19T19:30',
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
           },
           {
             'id': uuid.v4(),
-            'description': 'Purple Night #2',
-            'start_time': '2023-04-08T19:30',
-            'end_time': '2023-04-09T07:30',
-            'employee_id': '4c7e6819-a91a-45b2-a454-f931e4a7cce3'
-          },
-          //ORANGE
-          {
-            'id': uuid.v4(),
-            'description': 'Orange Day #1',
-            'start_time': '2023-04-07T07:30',
-            'end_time': '2023-04-07T19:30',
-            'employee_id': 'd5c4daa3-c726-426a-aa98-fb40f3fba816'
-          },
-          //BLUE
-          {
-            'id': uuid.v4(),
-            'description': 'Blue Night #1',
-            'start_time': '2023-04-01T19:30',
-            'end_time': '2023-04-02T07:30',
-            'employee_id': 'ca3827f0-375a-4891-aaa5-f5e8a5bad225'
+            'description': 'Day',
+            'start_time': '2023-04-20T07:30',
+            'end_time': '2023-04-20T19:30',
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
           },
           {
             'id': uuid.v4(),
-            'description': 'Blue Night #2',
-            'start_time': '2023-04-02T19:30',
-            'end_time': '2023-04-03T07:30',
-            'employee_id': 'ca3827f0-375a-4891-aaa5-f5e8a5bad225'
-          },]
+            'description': 'Night',
+            'start_time': '2023-04-21T19:30',
+            'end_time': '2023-04-22T07:30',
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
+          },
+          {
+            'id': uuid.v4(),
+            'description': 'Night',
+            'start_time': '2023-04-22T19:30',
+            'end_time': '2023-04-23T07:30',
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
+          },
+          {
+            'id': uuid.v4(),
+            'description': 'Day',
+            'start_time': '2023-04-28T07:30',
+            'end_time': '2023-04-28T19:30',
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
+          },
+          {
+            'id': uuid.v4(),
+            'description': 'Day',
+            'start_time': '2023-04-29T07:30',
+            'end_time': '2023-04-29T19:30',
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
+          },
+          {
+            'id': uuid.v4(),
+            'description': 'Night',
+            'start_time': '2023-04-30T19:30',
+            'end_time': '2023-005-01T07:30',
+            'employee_id': '79e02eb5-17c0-4dd9-bf55-aec03434812c'
+          },
+          //Luna Lovegood
+          {
+            'id': uuid.v4(),
+            'description': 'Day',
+            'start_time': '2023-04-08T07:30',
+            'end_time': '2023-04-08T19:30',
+            'employee_id': '861c4cde-2f0f-4796-8d8f-9492e74b2573'
+          },
+          {
+            'id': uuid.v4(),
+            'description': 'Day',
+            'start_time': '2023-04-16T07:30',
+            'end_time': '2023-04-16T19:30',
+            'employee_id': '861c4cde-2f0f-4796-8d8f-9492e74b2573'
+          },
+          {
+            'id': uuid.v4(),
+            'description': 'Day',
+            'start_time': '2023-04-17T07:30',
+            'end_time': '2023-04-17T19:30',
+            'employee_id': '861c4cde-2f0f-4796-8d8f-9492e74b2573'
+          },
+          {
+            'id': uuid.v4(),
+            'description': 'Day',
+            'start_time': '2023-04-24T07:30',
+            'end_time': '2023-04-24T19:30',
+            'employee_id': '861c4cde-2f0f-4796-8d8f-9492e74b2573'
+          },
+          {
+            'id': uuid.v4(),
+            'description': 'Day',
+            'start_time': '2023-04-27T07:30',
+            'end_time': '2023-04-27T19:30',
+            'employee_id': '861c4cde-2f0f-4796-8d8f-9492e74b2573'
+          },
+          ]
       )
     })
 };
